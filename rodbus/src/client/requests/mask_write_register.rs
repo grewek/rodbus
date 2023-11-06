10011dc0bfc6e214ade56154afee2e64487dcf4f
Kay Grewe <Kay.Grewe@iniationware.com>
2023-11-06 13:13:33 +0100

    feat: add mask_write_register to project, add MaskWriteRegister struct

---
+++ rodbus/src/client/requests/mask_write_register.rs
@@ -0,0 +1,10 @@
+#[derive(Debug,PartialEq)]
+pub(crate) struct MaskWriteRegister {
+    
+}
+
+impl std::fmt::Display for MaskWriteRegister {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
