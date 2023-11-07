44c950d5248dee3c2f09af350c966c7cf676c7e5
Kay Grewe <Kay.Grewe@iniationware.com>
2023-11-07 11:55:48 +0100

    feat: make and, or mask public for current crate

--- rodbus/src/client/requests/mask_write_register.rs
+++ rodbus/src/client/requests/mask_write_register.rs
@@ -1,7 +1,13 @@
+use scursor::{WriteCursor, ReadCursor};
+
+use crate::{RequestError, Indexed};
+
+use super::write_single::SingleWriteOperation;
+
 #[derive(Debug,PartialEq,Copy,Clone)]
 pub struct MaskWriteRegister {
-    and_mask: u16,
-    or_mask: u16,
+    pub(crate) and_mask: u16,
+    pub(crate) or_mask: u16,
 }
 
 impl std::fmt::Display for MaskWriteRegister {
