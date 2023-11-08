744c021c4ae8cd62ae6fcf5ca71636c001fc9755
Kay Grewe <Kay.Grewe@iniationware.com>
2023-11-08 12:07:11 +0100

    add masking operation and a constructor to MaskWriteRegister

--- rodbus/src/client/requests/mask_write_register.rs
+++ rodbus/src/client/requests/mask_write_register.rs
@@ -6,6 +6,22 @@ pub struct MaskWriteRegister {
     pub(crate) or_mask: u16,
 }
 
+impl MaskWriteRegister {
+    /// Create a new MaskWriteRegister structure with the specified masks
+    pub fn new(and_mask: u16, or_mask: u16) -> Self {
+        Self {
+            and_mask,
+            or_mask,
+        }
+    }
+
+    /// Value will be masked with the Values from the Struct. This is the default mask operation
+    /// from the Modbus Specifications Manual see Page 36 MODBUS Application Protocol 1.1b
+    pub fn mask_value(&self, value: u16) -> u16 {
+        (value & self.and_mask) | (self.or_mask & (!self.and_mask))
+    }
+}
+
 impl std::fmt::Display for MaskWriteRegister {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "AND_MASK: {:#016b} OR_MASK: {:#016b}", self.and_mask, self.or_mask)
