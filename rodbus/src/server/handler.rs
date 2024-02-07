3849635d45a95a99f7a95fcd49b9d898af5b9224
Kay Grewe <Kay.Grewe@iniationware.com>
2024-02-07 13:56:57 +0100

    refactor(handler): change return type of req handler to ServerDeviceInfo

--- rodbus/src/server/handler.rs
+++ rodbus/src/server/handler.rs
@@ -64,7 +64,7 @@ pub trait RequestHandler: Send + 'static {
         _mei_code: MeiCode,
         _read_dev_id: ReadDeviceCode,
         _object_id: Option<u8>,
-    ) -> Result<DeviceInfo, ExceptionCode> {
+    ) -> Result<ServerDeviceInfo, ExceptionCode> {
         Err(ExceptionCode::IllegalFunction)
     }
 
